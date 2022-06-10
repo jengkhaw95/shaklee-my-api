3603f3774bbbe3cd438bd7fb85ae8955dba8459c
Jeng Khaw <jengkhaw95@gmail.com>
2022-06-10 11:58:27 +0800

    added GET /ip endpoint to check proxy layer on prod

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import "dotenv/config";
-import express, {Application, Request, Response} from "express";
+import express, {Application} from "express";
+import rateLimiter from "./rateLimiter";
 import product from "./router/product";
 
 const app: Application = express();
 
 const port = process.env.PORT || 3001;
 
-app.use("/product", product);
+const rateLimit = rateLimiter({max: 3, durationMs: 10 * 1000});
+
+app.set("trust proxy", 3);
+
+app.get("/ip", rateLimit, (req, res) => {
+  res.send(req.ip);
+});
+
+app.use("/product", rateLimit, product);
 
 app.listen(port, function () {
   console.log(`App is listening on port ${port} !`);
