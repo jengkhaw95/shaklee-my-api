536accd7940af31d5f41e1d9bc47457dac7864dd
Jeng Khaw <jengkhaw95@gmail.com>
2022-06-10 17:36:26 +0800

    added logging when new data is stored

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -5,6 +5,17 @@ import "dotenv/config";
 import {connectToDB} from "../db";
 import Shaklee from "./shaklee";
 
+const logUpdatesToDb = async (document: any[]) => {
+  const formattedDoc = document.map((d) => ({
+    ...d,
+    ts: Date.now(),
+    product_no: d.product_no,
+  }));
+
+  const db = await connectToDB();
+  return await db.collection("updateLogs").insertMany(formattedDoc);
+};
+
 (async () => {
   const shaklee = new Shaklee({
     username: process.env.SHAKLEE_ID!,
@@ -18,9 +29,11 @@ import Shaklee from "./shaklee";
   const productsToInsert = products
     .filter((p) => p.product_no && !productIdsFromDB.includes(p.product_no))
     .map((d) => ({...d, _id: d.product_no}));
+
   if (productsToInsert.length > 0) {
     const insertResponse = await productCollection.insertMany(productsToInsert);
     if (insertResponse.acknowledged) {
+      await logUpdatesToDb(productsToInsert);
       console.log(`Inserted ${insertResponse.insertedCount} product(s)`);
     }
   }
