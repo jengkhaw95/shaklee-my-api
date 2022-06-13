897207d8edb678baf6b6b5d2b5bbb4b353c405fe
Jeng Khaw <jengkhaw95@gmail.com>
2022-06-13 23:55:22 +0800

    added static folder on server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import express, {Application} from "express";
+import path from "path";
 import api from "./router/api";
 
 const app: Application = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname, "../docs/output")));
 
 // 3 is the number of proxy layer on Heroku
 app.set("trust proxy", 3);
@@ -12,11 +15,10 @@ app.set("trust proxy", 3);
 api(app);
 
 // Used to check ip
-
 //app.get("/ip", (req, res) => {
 //  res.send(req.ip);
 //});
 
 app.listen(port, function () {
-  console.log(`App is listening on port ${port} !`);
+  console.log(`App is listening on port ${port}!`);
 });
