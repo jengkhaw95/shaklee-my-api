b5e87c5427432a8431d96477e5786fdbe4e3d38e
Jeng Khaw <jengkhaw95@gmail.com>
2022-06-20 22:54:22 +0800

    added telegram endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express, {Application} from "express";
 import path from "path";
 import api from "./router/api";
+import telegram from "./router/telegram";
 
 const app: Application = express();
 
@@ -12,6 +13,7 @@ app.use(express.static(path.resolve(__dirname, "../docs/output")));
 // 3 is the number of proxy layer on Heroku
 app.set("trust proxy", 3);
 
+telegram(app);
 api(app);
 
 // Used to check ip
