94cf3fd1970add24300a0914d4955399354d615a
Jeng Khaw <jengkhaw95@gmail.com>
2022-12-13 21:41:43 +0800

    fix bulk write inefficiency when syncing data

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -2,6 +2,7 @@
 // Job: update data to database
 
 import "dotenv/config";
+import {AnyBulkWriteOperation} from "mongodb";
 import {connectToDB} from "../db";
 import {parseProductInfo} from "../router/telegram";
 import {tbot} from "../telegram";
@@ -21,6 +22,11 @@ export const workerUpdateProducts = async () => {
 
   const products = await shaklee.getProducts();
 
+  //console.log(
+  //  "VITA C 2",
+  //  products.find((p) => p.product_no === "00P729")
+  //);
+
   const db = await connectToDB();
   const productCollection = await db.collection("products");
 
@@ -35,72 +41,193 @@ export const workerUpdateProducts = async () => {
     return isPass;
   });
 
-  const {toArchive, toUpdate} = allProductsFromDatabase.reduce<{
-    toArchive: any[];
-    toUpdate: any[];
+  const {newProducts, changedProducts} = productsToInsert.reduce<{
+    newProducts: any[];
+    changedProducts: any[];
+    remainingProducts: any[];
   }>(
     (a, b) => {
-      const matchedItem = productsToInsert.find((bn) => bn._id === b._id);
-      if (!matchedItem) {
-        if (b.status !== "archived") {
-          a.toArchive.push(b);
-        }
-        return a;
-      }
-      let isToBeModify = false;
-      for (let k in Object.keys(b)) {
-        const v1 = b[k];
-        const v2 = matchedItem[k];
-        if (typeof v1 == "object") {
-          if (JSON.stringify(v1) !== JSON.stringify(v2)) {
-            isToBeModify = true;
-            break;
-          }
-        } else {
-          if (v1 !== v2) {
-            isToBeModify = true;
-            break;
-          }
-        }
-      }
-      if (isToBeModify) {
-        a.toUpdate.push(b);
+      const existingProduct = allProductsFromDatabase.find(
+        (pr) => pr._id === b._id
+      );
+      if (!existingProduct) {
+        a.newProducts.push(b);
+      } else if (
+        existingProduct.status !== b.status ||
+        JSON.stringify(existingProduct.dn) !== JSON.stringify(b.dn)
+      ) {
+        a.changedProducts.push(b);
+      } else {
+        a.remainingProducts.push(b);
       }
       return a;
     },
-    {toArchive: [], toUpdate: []}
+    {newProducts: [], changedProducts: [], remainingProducts: []}
   );
 
-  const updateMany = {
-    filter: {_id: {$nin: toArchive.map((d) => d._id)}},
+  const {oosProducts, promotionProducts} = changedProducts.reduce<{
+    oosProducts: any[];
+    promotionProducts: any[];
+  }>(
+    (a, b) => {
+      if (b.status === "promotion") {
+        a.promotionProducts.push(b);
+      }
+
+      if (b.status === "oos") {
+        a.promotionProducts.push(b);
+      }
+      return a;
+    },
+    {
+      oosProducts: [],
+      promotionProducts: [],
+    }
+  );
+
+  const toArchive_ = allProductsFromDatabase.filter(
+    (p) =>
+      !productsToInsert.some(
+        (r) => r.status !== "archived" && r._id == p._id
+      ) && p.status !== "archived"
+  );
+
+  const updateMany_ = {
+    filter: {_id: {$in: toArchive_.map((d) => d._id)}},
     update: {$set: {status: "archived", lastUpdateAt: Date.now()}},
   };
 
-  const replaceMany = toUpdate.map((up) => ({
+  const replaceMany_ = [...oosProducts, ...promotionProducts].map((up) => ({
     replaceOne: {
       filter: {_id: up._id},
       replacement: up,
     },
   }));
 
-  const insertMany = toAdd.map((ad) => ({
+  const insertMany_ = newProducts.map((ad) => ({
     insertOne: {
       document: ad,
     },
   }));
 
-  const bulkWriteOperations: any[] = [...replaceMany, ...insertMany];
-  if (toArchive.length) {
-    bulkWriteOperations.push({updateMany});
+  let bulkWriteOps: any[] = [];
+
+  if (toArchive_.length) {
+    console.log("Archive", toArchive_.length);
+    bulkWriteOps.push({updateMany: updateMany_});
+  }
+  if (replaceMany_.length) {
+    console.log("Replace", replaceMany_.length);
+    bulkWriteOps = bulkWriteOps.concat(replaceMany_);
+  }
+  if (newProducts.length) {
+    console.log("New", newProducts.length);
+    bulkWriteOps = bulkWriteOps.concat(insertMany_);
   }
 
-  if (!bulkWriteOperations.length) {
+  if (!bulkWriteOps.length) {
     console.log("No changes in products found!");
     return;
   }
-  const res = await productCollection.bulkWrite(bulkWriteOperations, {
+
+  const res = await productCollection.bulkWrite(bulkWriteOps, {
     ordered: false,
   });
+  console.log(res);
+
+  //const {toArchive, toUpdate} = allProductsFromDatabase.reduce<{
+  //  toArchive: any[];
+  //  toUpdate: any[];
+  //}>(
+  //  (a, b) => {
+  //    let isLogging = (b._id as unknown as string) === "00P729";
+  //    // The true item is in database already
+  //    const matchedItem = productsToInsert.find((bn) => bn._id === b._id);
+  //    //if (isLogging) {
+  //    //  console.log({matchedItem});
+  //    //}
+  //    if (!matchedItem) {
+  //      if (b.status !== "archived") {
+  //        a.toArchive.push(b);
+  //      }
+  //      return a;
+  //    }
+  //    //if (isLogging) {
+  //    //  console.log(b.status, matchedItem.status);
+  //    //  console.log(b);
+  //    //  console.log(matchedItem);
+  //    //}
+  //    if (matchedItem.status !== b.status) {
+  //      a.toUpdate.push(matchedItem);
+  //      return a;
+  //    }
+  //    return a;
+  //    let isToBeModify = false;
+
+  //    if (isLogging) {
+  //      console.log(Object.keys(b));
+  //    }
+  //    for (let k of Object.keys(b)) {
+  //      const v1 = b[k];
+  //      const v2 = matchedItem[k];
+  //      if (isLogging) {
+  //        console.log({v1, v2});
+  //      }
+  //      if (typeof v1 == "object") {
+  //        if (JSON.stringify(v1) !== JSON.stringify(v2)) {
+  //          isToBeModify = true;
+  //          break;
+  //        }
+  //      } else {
+  //        if (v1 !== v2) {
+  //          isToBeModify = true;
+  //          break;
+  //        }
+  //      }
+  //    }
+
+  //    if (isLogging) {
+  //      console.log({isToBeModify});
+  //    }
+  //    if (isToBeModify) {
+  //      a.toUpdate.push(b);
+  //    }
+  //    return a;
+  //  },
+  //  {toArchive: [], toUpdate: []}
+  //);
+
+  //const updateMany = {
+  //  filter: {_id: {$nin: toArchive.map((d) => d._id)}},
+  //  update: {$set: {status: "archived", lastUpdateAt: Date.now()}},
+  //};
+
+  //const replaceMany = toUpdate.map((up) => ({
+  //  replaceOne: {
+  //    filter: {_id: up._id},
+  //    replacement: up,
+  //  },
+  //}));
+
+  //const insertMany = toAdd.map((ad) => ({
+  //  insertOne: {
+  //    document: ad,
+  //  },
+  //}));
+
+  //const bulkWriteOperations: any[] = [...replaceMany, ...insertMany];
+  //if (toArchive.length) {
+  //  bulkWriteOperations.push({updateMany});
+  //}
+
+  //if (!bulkWriteOperations.length) {
+  //  console.log("No changes in products found!");
+  //  return;
+  //}
+  //const res = await productCollection.bulkWrite(bulkWriteOperations, {
+  //  ordered: false,
+  //});
+  //console.log(res);
 
   //const outdatedProducts = await productCollection
   //  .find({
@@ -138,16 +265,16 @@ export const workerUpdateProducts = async () => {
       .find({})
       .toArray();
 
-    if (toAdd.length) {
-      console.table([toAdd]);
-      const promises = toAdd.map((product) =>
-        tbot.broadcast(
-          subscriptionIds.map((d) => d.chatId),
-          parseProductInfo(product, true)
-        )
-      );
-      await Promise.all(promises);
-    }
+    //if (toAdd.length) {
+    //  console.table([toAdd]);
+    //  const promises = toAdd.map((product) =>
+    //    tbot.broadcast(
+    //      subscriptionIds.map((d) => d.chatId),
+    //      parseProductInfo(product, true)
+    //    )
+    //  );
+    //  await Promise.all(promises);
+    //}
 
     await tbot.broadcast(
       subscriptionIds.map((d) => d.chatId),
